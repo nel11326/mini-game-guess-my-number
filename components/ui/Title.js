d8b11cd16c0bb691d23f1df8e82e2d35b022dfe0
nel11326 <lenor11326@hotmail.com>
2024-08-17 13:36:57 +0800

    font(add): add custome fonts from expo

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -8,8 +8,8 @@ export default Title;
 
 const style = StyleSheet.create({
   title: {
+    fontFamily: "open-sans-bold",
     fontSize: 24,
-    fontWeight: "bold",
     color: "white",
     textAlign: "center",
     borderWidth: 2,
